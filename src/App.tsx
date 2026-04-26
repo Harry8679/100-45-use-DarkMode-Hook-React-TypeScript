97246d67c336b337da58658809e0084b87abffdd
Harry7986 <emarh.harry.code@gmail.com>
2026-04-27 00:22:42 +0200

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import DarkModeDemo from './components/DarkModeDemo';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <DarkModeDemo />;
 }
 
-export default App
+export default App;
